f94bf8f72e248c42a2edf147bd384bc6cf2c1cfe
Deekshitha Reddy Manda <deekshareddy3508@gmail.com>
2026-04-24 11:39:32 +0530

    fixed cloudinary file case

--- Backend for blog app/APIs/commonAPI.js
+++ Backend for blog app/APIs/commonAPI.js
@@ -7,7 +7,7 @@ import { verifyToken } from '../middlewares/verifyToken.js'
 const {sign}=jwt
 export const commonApp=exp.Router()
 import { upload } from '../Config/multer.js'
-import { uploadToCloudinary } from "../config/cloudinaryUpload.js";
+import { uploadToCloudinary } from "../Config/cloudinaryupload.js";
 import cloudinary from "../config/cloudinary.js";
 config()
 // Route for register
