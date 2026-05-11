4c8a3df90da50d295ee7f7aa4d7716b1305ee999
Deekshitha Reddy Manda <deekshareddy3508@gmail.com>
2026-05-11 14:09:10 +0530

    Update ForgotPassword.jsx

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -30,7 +30,7 @@ function ForgotPassword() {
       setLoading(true);
 
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/common-api/forgot-password`,
+        `${import.meta.env.VITE_API_URL}/auth/forgot-password`,
         data
       );
 
